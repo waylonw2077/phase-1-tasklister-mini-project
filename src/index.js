97a2a808b71ea1292ef3d667fa7fc6b05695d302
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-23 15:11:19 -0400

    completes assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+    const form = document.querySelector("#task-form");
+  form.addEventListener("submit", addTask);
 });
+const form = document.querySelector('#task-form');
+    const taskInput = document.querySelector('#task-input');
+    const taskList = document.querySelector('#task-list');
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const taskText = taskInput.value;
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+      taskInput.value = '';
+    });
